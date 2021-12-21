c5ccca66361e5a6de60dec0433df0c54d2071fe2
Elliot Chung <ec2018@gmail.com>
2021-12-21 01:14:30 -0800

    added towers and styling for them

--- src/App.js
+++ src/App.js
@@ -57,10 +57,31 @@ function ControlBar({height, setHeight, mute, setMute, solving, setSolving}) {
 
 function Display(props) {
   return (
-    <div>
-      <Block tHeight="3" width="1" tower="1"/>
-      <Block tHeight="3" width="2" tower="1"/>
-      <Block tHeight="3" width="3" tower="1"/>
+    <div className="display">
+      <div className="first tower">
+        <Block tHeight="6" width="1" tower="1"/>
+        <Block tHeight="6" width="2" tower="1"/>
+        <Block tHeight="6" width="3" tower="1"/>
+        <Block tHeight="6" width="4" tower="1"/>
+        <Block tHeight="6" width="5" tower="1"/>
+        <Block tHeight="6" width="6" tower="1"/>
+      </div>
+      <div className="second tower">
+        <Block tHeight="6" width="1" tower="1"/>
+        <Block tHeight="6" width="2" tower="1"/>
+        <Block tHeight="6" width="3" tower="1"/>
+        <Block tHeight="6" width="4" tower="1"/>
+        <Block tHeight="6" width="5" tower="1"/>
+        <Block tHeight="6" width="6" tower="1"/>
+      </div>
+      <div className="third tower">
+        <Block tHeight="6" width="1" tower="1"/>
+        <Block tHeight="6" width="2" tower="1"/>
+        <Block tHeight="6" width="3" tower="1"/>
+        <Block tHeight="6" width="4" tower="1"/>
+        <Block tHeight="6" width="5" tower="1"/>
+        <Block tHeight="6" width="6" tower="1"/>
+      </div>
     </div>
   )
 }
@@ -68,9 +89,13 @@ function Display(props) {
 function Block({tHeight, width}) {
   const pWidth = 1000 + ((width-1)/(tHeight-1))*3000;
   return (
-    <svg viewBox="0 0 4000 1000">
+    <svg viewBox={`0 0 4000 1000`} className="block" >
       <rect
-        x="0" y="0" 
+        x={2000-pWidth/2} y="0" 
+        fill="red"
+        stroke="black"
+        strokeWidth="15"
+        radius="20"
         width={pWidth} height="100%" />
     </svg>
   )
