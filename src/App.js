79130716d309df50980ce7ca5122e721cd104159
Elliot Chung <ec2018@gmail.com>
2021-12-24 19:18:26 -0800

    Began writing the solving algorithm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import './index.css';
 
 function App() {
@@ -7,9 +7,9 @@ function App() {
   const [solving, setSolving] = useState(false);
   const [done, setDone]       = useState(false);
 
-  if (done) {
-    setTimeout(() => {setSolving(false)}, 2000);
-  }
+  // if (done) {
+  //   setTimeout(() => {setSolving(false)}, 2000);
+  // }
 
   return (
     <div className="App">
@@ -26,6 +26,7 @@ function App() {
             height={height}
             solving={solving}
             setDone={setDone}
+            setSolving={setSolving}
           />
         </main>
     </div>
@@ -47,41 +48,99 @@ function ControlBar({height, setHeight, mute, setMute, solving, setSolving}) {
     <form onSubmit={solve} className="sidebar">
       <label className="height">
         <p>Tower Height: {height}</p>
-        <input type="range" min="3" max="88" value={height} onChange={(event)=>{setHeight(event.target.value)}}/>
+        <input 
+          type="range" 
+          min="3" 
+          max="88" 
+          value={height} 
+          onChange={(event)=>{setHeight(event.target.value)}}
+        />
       </label>
       <label className="mute">
         <span>Mute: </span> 
-        <input type="checkbox" checked={mute} onChange={(event)=>{setMute(event.target.checked)}}/>
+        <input 
+          type="checkbox" 
+          checked={mute} 
+          onChange={(event)=>{setMute(event.target.checked)}}
+        />
       </label>          
       <input type="submit" value="Solve" disabled={solving}/>
     </form>
   );
 }
 
-function Display({height, solving, setDone}) {
-  if (solving) setDone(true); 
+function Display({height, solving, setDone, setSolving}) {
+  const t1 = [];
+  for (let i=1; i<=height; i++) t1.push(i);
+  const t2 = t1.map(()=>{return 0});
+  const t3 = t2;
+
+  const [tArr1, setArr1] = useState(t1);
+  const [tArr2, setArr2] = useState(t2);
+  const [tArr3, setArr3] = useState(t3);
+
+  const moveTower = useCallback((mHeight, source, setSource, dest, setDest, temp, setTemp) => {
+    if (mHeight === 1) {
+      const blockInd = source.findIndex(width => width !== 0);
+      let copyArr = source;
+      copyArr[blockInd] = 0;
+      setSource(copyArr);
+      return;
+    }
+    moveTower(mHeight-1, source, setSource, temp, setTemp, dest, setDest);
+  }, []);
+
+  useEffect(()=>{
+    setArr1(t1);
+    setArr2(t2);
+    setArr3(t3);
+  }, [height]);
   
-  const arr = [];
-  for (let i=1; i<=height; i++) arr.push(i);
+  useEffect(() => {
+    if (solving) {
+      moveTower(height, tArr1, setArr1, tArr2, setArr2, tArr3, setArr3);
+      setSolving(false);
+      setDone(true); 
+    }
+  }, [solving, moveTower, height, tArr1, tArr2, tArr3, setSolving, setDone]);
+
+
+  const firstTower = tArr1.map((width, index)=>{
+    return <Block tHeight={height} width={width} key={index+"t1"}/>
+  })
+  const secondTower = tArr2.map((width, index)=>{
+    return <Block tHeight={height} width={width} key={index+"t2"}/>
+  })
+  const thirdTower = tArr3.map((width, index)=>{
+    return <Block tHeight={height} width={width} key={index+"t3"}/>
+  })
+
   return (
     <div className="display">
       <div className="first tower">
-        {arr.map((width)=>{
-          return <Block tHeight={height} width={width} key={width}/>
-        })}
+        {firstTower}
       </div>
       <div className="second tower">
+        {secondTower}
       </div>
       <div className="third tower">
+        {thirdTower}
       </div>
     </div>
   )
 }
 
+
 function Block({tHeight, width}) {
-  const pWidth = 1000 + ((width-1)/(tHeight-1))*3000;
+
+  const pWidth = (width===0 ? 0 : 1000 + ((width-1)/(tHeight-1))*3000);
   return (
-    <svg viewBox={`0 0 4000 1000`} preserveAspectRatio="none" className="block" key={width}>
+    <svg 
+      viewBox={`0 0 4000 1000`} 
+      preserveAspectRatio="none" 
+      className={width===0 ? "emptyBlock": "block"} 
+      key={width+"cnt"}
+    >
       <rect
         x={2000-pWidth/2} y="0" 
         fill="red"
@@ -89,7 +148,7 @@ function Block({tHeight, width}) {
         strokeWidth="15"
         radius="20"
         width={pWidth} height="100%" 
-        key={width}/>
+        key={width+"rect"}/>
     </svg>
   )
 }
